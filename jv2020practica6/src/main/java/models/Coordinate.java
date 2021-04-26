6f72ffe68e7590e014b70e7e138080eedd85c748
FranMoraton <franmoraton@gmail.com>
2021-04-26 10:57:09 +0200

    26/04/2021 Clase Coordinate

---
+++ jv2020practica6/src/main/java/models/Coordinate.java
@@ -0,0 +1,35 @@
+package models;
+
+public class Coordinate {
+
+	private int x;
+	private int y;
+	
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+		if (x >= 0) {
+			this.x = x;
+		}
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+		if (y >= 0) {
+			this.y = y;
+		}
+	}
+	
+}
